515fb246aab28e3fe23000e9fcffce3f381cdcbf
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-24 14:48:34 -0700

    feat: export createApp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { MODULE_NAME } from "./constants.js";
+import { createApp } from "petite-vue"
+import { CANONICAL_NAME, MODULE_NAME } from "./constants.js";
+
+const api = {
+    createApp,
+}
 
 Hooks.on('init', async () => {
     console.log(`${MODULE_NAME} | Initializing ${MODULE_NAME}`);
+    game.modules.get(CANONICAL_NAME).api = api
+    Hooks.callAll(`${CANONICAL_NAME}.init`, api);
 });
+
+Hooks.on('ready', async () => {
+    console.log(`${MODULE_NAME} | Ready`);
+    Hooks.callAll(`${CANONICAL_NAME}.ready`, api);
+})
