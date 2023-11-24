c31702c49f144bd318b9a0b12d7b3ae91dceccb8
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-24 09:30:39 -0700

    chore: setup project

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const CANONICAL_NAME = 'fvtt-module-template';
-export const MODULE_NAME = 'FVTT Module Template';
+export const CANONICAL_NAME = 'petitevue-lib';
+export const MODULE_NAME = 'Petite Vue Lib';
 export const MODULE_PATH = `modules/${CANONICAL_NAME}`;
 export const TEMPLATE_PATH = `${MODULE_PATH}/templates`;
